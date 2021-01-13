f40844b419f4a138c9d1890a068c3b43687a3092
Meenakshi Asokan <masokan94@gmail.com>
2021-01-12 19:10:35 -0500

    Added plots of exemplars and used the function to calculate tau

--- Compute_soundevoked_response_timescale.m
+++ Compute_soundevoked_response_timescale.m
@@ -1,8 +1,45 @@
 close all
 clearvars;
 data_dir = 'F:\EPHYS\Curr Bio\Dataset\Figure_1';
-load(fullfile(data_dir,'noiseburst.mat'));
+load(fullfile(data_dir,'noiseburst_ex.mat'));
+num_regions = length(noiseburst_examples);
+sm = 5;
+for r_num = 1:num_regions
+    if (r_num ==1)
+    color = 'r';
+    elseif (r_num ==2)
+    color = [0,0.5,0];
+    else
+    color = 'b';
+    end
+    raster = noiseburst_examples(r_num).raster;
+    figure(1);
+    subplot(1,4,r_num);
+    L = logical(raster);
+    MarkerFormat.Color = color;
+    plotSpikeRaster(L,'PlotType','scatter','MarkerFormat',MarkerFormat);
+    box off
+    xlabel('Time wrt sound onset (ms)');
+    ylabel('Trial number');
+    set(gca,'fontsize',12);
+    
+    [tau_single, tau_final, curve] = myfunc_calculate_tau(raster,sm);
+    subplot(1,4,4);
+    sm_resp = smoothdata(mean(raster,1),'gaussian',sm);
+    [c,lags] = xcorr(sm_resp,'coeff');
+    x = lags((length(lags)+1)/2:end);
+    y = c((length(lags)+1)/2:end);
+    plot(x,y,'color',color,'LineStyle','--');
+    hold on
+    plot(curve,'k');
+    hold on
+    box off
+    xlabel('Time lag (ms)');
+    ylabel('Autocorrelation');
+    set(gca,'fontsize',12);
+end
 %%
+load(fullfile(data_dir,'noiseburst.mat'));
 num_regions = length(noiseburst);
 sm = 5;
 tau_single = cell(1,num_regions);
@@ -11,38 +48,13 @@ for r_num = 1:num_regions
     all_rasters = noiseburst(r_num).raster;
     for i = 1:size(all_rasters,3)
         raster = all_rasters(:,:,i);
-        sm_resp = smoothdata(mean(raster,1),'gaussian',sm);
-        [c,lags] = xcorr(sm_resp,'coeff');
-        x = lags((length(lags)+1)/2:end);
-        y = c((length(lags)+1)/2:end);
-        %Single exponential fit
-        options = fitoptions('exp1');
-        options.StartPoint = [1 -0.5];
-        options.Upper = [1 0];
-        options.Lower = [1 -Inf];
-        [curve,gof] = fit(x',y','exp1',options);
-        tau_single{r_num}(i) = -1/(curve.b);
-        %Double exponential fit if goodness of fit is low
-        if (gof.adjrsquare<0.75)
-            options = fitoptions('exp2');
-            options.StartPoint = [1 -0.5 0.5 -0.3];
-            options.Upper = [Inf 0 Inf -0.01];
-            [curve,gof] = fit(x',y','exp2',options);
-            tau1 = -1/(curve.b);
-            tau2 = -1/(curve.d);
-            tau_final{r_num}(i) = (curve.a*tau1+curve.c*tau2)/(curve.a+curve.c);
-            if (tau_final{r_num}(i)>100)
-                tau_final{r_num}(i) = tau_single{r_num}(i);
-            end
-        else
-            tau_final{r_num}(i) = tau_single{r_num}(i);
-        end
+        [tau_single{r_num}(i), tau_final{r_num}(i), curve] = myfunc_calculate_tau(raster,sm);
     end
     r_num
 end
-%%
+
 figure();
-my_boxplot(tau_final);
+myfunc_boxplot(tau_final);
 box off
 xticklabels({'IC','MGB','A1'});
 ylabel('Neural timescale, tau (ms)');
